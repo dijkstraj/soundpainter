6ea1cab82566cb9d0113e653cd624e0cfcf30362
Jogchem Dijkstra <jogchem@gmail.com>
2018-04-28 15:06:26 +0200

    Test mathjs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,24 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+import * as math from 'mathjs';
+
+describe('math', () => {
+  it('bla', () => {
+    const points = [
+      [0, 0],
+      [1, 1],
+      [2, 1],
+      [3, 4]
+    ];
+    const matrix = [
+      [ 0,  2,  0,  0],
+      [-1,  0,  1,  0],
+      [ 2, -5,  4, -1],
+      [-1,  3, -3,  1]
+    ];
+    const t = 0.95;
+    const step1 = math.multiply(0.5, [1.0, t, t * t, t * t * t]);
+    const step2 = math.multiply(step1, matrix);
+    console.log(math.multiply(step2, points));
+  });
 });
